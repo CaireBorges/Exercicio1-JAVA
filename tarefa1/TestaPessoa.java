36607a252e1a10b84142b706fccc599b1682a7c3
CaireBorges <155997421+CaireBorges@users.noreply.github.com>
2024-12-06 10:51:46 -0300

    Add files via upload

---
+++ tarefa1/TestaPessoa.java
@@ -0,0 +1,20 @@
+package tarefa1;
+
+public class TestaPessoa {
+	
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa("123","João");
+		Pessoa p2 = new Pessoa("111","Maria");
+		
+		System.out.println("Dados da primeira pessoa");
+		System.out.println(p1);
+		
+		System.out.println("-------------------------");
+		
+		System.out.println("Dados da segunda pessoa");
+		System.out.println(p2);
+		
+		
+	}
+}
